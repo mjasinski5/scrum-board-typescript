d4364f7b7570de5f726be1622517db8894d7c85d
Jasinski, Mariusz <mariusz.jasinski@sap.com>
2019-02-05 19:57:52 +0100

    Add docs folder for Github pages

---
+++ docs/precache-manifest.bafe4658c724ddee8d4cdc3685a1b364.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2a8fb1e03e03d66365e2",
+    "url": "/static/js/main.2a8fb1e0.chunk.js"
+  },
+  {
+    "revision": "ce74f729666be7113abc",
+    "url": "/static/js/1.ce74f729.chunk.js"
+  },
+  {
+    "revision": "3ba8c307da487bc06df7366b32105e6e",
+    "url": "/index.html"
+  }
+];
