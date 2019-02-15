3d2651e8ab1a233960b6bfa0d2c3a5a5989a2325
Jasinski, Mariusz <mariusz.jasinski@sap.com>
2019-02-15 15:14:06 +0100

    Updates

---
+++ precache-manifest.7d19db3174305292758d449b6e523a1d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "33cf470f451701c8da4b",
+    "url": "/scrum-board-typescript/static/js/runtime~main.33cf470f.js"
+  },
+  {
+    "revision": "1e14d0ede5748a199fa3",
+    "url": "/scrum-board-typescript/static/js/main.1e14d0ed.chunk.js"
+  },
+  {
+    "revision": "5c5b1fa5609947cfd0ea",
+    "url": "/scrum-board-typescript/static/js/1.5c5b1fa5.chunk.js"
+  },
+  {
+    "revision": "271dad205d701abc215ff57ffe8e693a",
+    "url": "/scrum-board-typescript/index.html"
+  }
+];
